22dcbecfcb332632b1997fc785ad5dba63ccfffb
Umar Khatab <umarkhatab467@gmail.com>
2023-10-24 22:25:19 +0500

    Delete backend directory

--- backend/app/routes/OneInch.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const OneInch = require("../controllers/OneInch.controller.js");
-  
-    var router = require("express").Router();
-    router.get("/",  OneInch.hello);
-
-    // router.post("/oneInch",);
-  };
-  
